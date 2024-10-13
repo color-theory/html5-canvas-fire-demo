06ad1b1904b2c6f76c1246a30bde222c79c0cf1b
Color <jgstringer@gmail.com>
2024-10-13 15:56:33 -0500

    pretty cool

--- fire.js
+++ fire.js
@@ -2,25 +2,61 @@ document.addEventListener("DOMContentLoaded", function () {
 	const canvas = document.getElementById('fireCanvas');
 	const ctx = canvas.getContext('2d');
 
-	const imageData = ctx.createImageData(canvas.width, canvas.height);
+	const width = canvas.width;
+	const height = canvas.height;
+	const imageData = ctx.createImageData(width, height);
 	let pixels = imageData.data;
 
+	const timingIntervalSlider = document.getElementById('timingInterval');
+	const magnitudeSlider = document.getElementById('magnitude');
+	const timingIntervalValue = document.getElementById('timingIntervalValue');
+	const magnitudeValue = document.getElementById('magnitudeValue');
+
 	let lastTime = 0;
-	let timingInterval = 100;
+	let timingInterval = parseInt(timingIntervalSlider.value, 10);
+	let magnitude = parseInt(magnitudeSlider.value, 10);
+
+	timingIntervalSlider.oninput = function () {
+		timingInterval = parseInt(this.value, 10);
+	}
+	magnitudeSlider.oninput = function () {
+		magnitude = parseInt(this.value, 10);
+	}
+
+	const reseed = () => {
+		for (let x = 0; x < width; x++) {
+			let index = (x * 4) + 0 * (width * 4);
+			pixels[index] = Math.floor(Math.random() * 255);
+			pixels[index + 1] = 0;
+			pixels[index + 2] = Math.floor(Math.random() * 50);
+			pixels[index + 3] = 255;
+		}
+	}
 
 	function updateCanvas(timestamp) {
 		if (timestamp - lastTime > timingInterval) {
-			for (let i = 0; i < pixels.length; i += 4) { // Step through rgba values
-				pixels[i] = Math.floor(Math.random() * 255);
-				pixels[i + 1] = 0;
-				pixels[i + 2] = Math.floor(Math.random() * 50);
-				pixels[i + 3] = 255;
+			for (let y = 1; y < height; y++) {
+				for (let x = 0; x < width; x++) {
+					let index = (x * 4) + y * (width * 4);
+					let aboveIndex = (x * 4) + (y - 1) * (width * 4);
+					let centerEffect = Math.abs(width / 2 - x) / (width / 2);
+					let coolingEffect = 0.1 + 0.9 * (Math.sin(Math.PI * centerEffect) ** 2);
+					let decay = Math.exp(-y / (height * coolingEffect * 10));
+					let factor = 1 + magnitude * decay; // Dynamic factor based on position and magnitude
+
+					pixels[index] = Math.max(0, (pixels[aboveIndex] * factor + pixels[index]) / (factor + .92) - 1);
+					pixels[index + 1] = 0;
+					pixels[index + 2] = Math.max(0, (pixels[aboveIndex + 2] * factor + pixels[index + 2]) / (factor + .92) - 1);
+					pixels[index + 3] = 255;
+				}
 			}
 			ctx.putImageData(imageData, 0, 0);
 			lastTime = timestamp;
+			reseed();
 		}
 		requestAnimationFrame(updateCanvas);
 	}
+	reseed();
 
 	requestAnimationFrame(updateCanvas);
 });
